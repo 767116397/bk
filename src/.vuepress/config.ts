8d7d864f943d04f309cdd9ef008a845e5970f465
earth_qi <767116397@qq.com>
2023-03-25 23:41:52 +0800

    init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,64 @@
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+import theme from "./theme.js";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    // "/": {
+    //   lang: "en-US",
+    //   title: "Docs Demo",
+    //   description: "A docs demo for vuepress-theme-hope",
+    // },
+    "/": {
+      lang: "zh-CN",
+      title: "文档演示",
+      description: "vuepress-theme-hope 的文档演示",
+    },
+  },
+  markdown: {
+    toc: {
+      shouldAllowNested: true,
+      level:[1, 2, 3, 4, 5, 6]
+    },
+    anchor: {
+      level:[1, 2, 3, 4, 5, 6],
+    },
+    headers: {
+      level: [1, 2, 3, 4, 5, 6]
+    }
+  },
+
+  
+  theme,
+
+  plugins: [
+    // 搜索
+    searchProPlugin({
+
+      locales: {
+        "/": {
+          placeholder: "开始搜索"
+        }
+      },
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => "page.frontmatter.category",
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => "page.frontmatter.tag",
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ]
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
---
+++ src/guide/foo/foo1.md
@@ -0,0 +1,48 @@
+---
+title: foo1
+icon: config
+---
+  asd
+
+
+# 1
+
+qwedqweqwe
+
+
+## 1.1
+
+asdasd
+
+asda
+as
+da
+sd
+asd
+as
+d
+asd
+### 1.1.1
+
+sasdadasd
+
+
+### 1.1.2
+
+asdasd
+as
+da
+sd
+
+
+#### 1.1.2.1
+
+# 2
+asdasd
+
+asd
+
+## 2.1
+
+asdsad
+
